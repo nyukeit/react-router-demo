2b47e373f8d527dcac8e099a7e17b128a9084c31
Nyukeit <nyukeit@outlook.com>
2023-12-06 13:27:33 +0100

    [Edit] Page titles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,7 @@ export default function About() {
 
   return (
     <div>
+      <h1>About Us</h1>
       <HomeButton />
     </div>
   )
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,6 +4,7 @@ export default function Contact() {
 
   return (
     <div>
+      <h1>Contact Us</h1>
       <HomeButton />
     </div>
   )
